6e372ccbf053ee085e9f0ab3820a5206fe008bdb
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-04-05 19:31:59 -0500

    Mapping through history array of moves to display historic moves

--- src/index.js
+++ src/index.js
@@ -79,6 +79,22 @@ const Square = (props) => {
       const history = this.state.history;
       const current = history[history.length - 1];
       const winner = calculateWinner(current.squares);
+
+      const moves = history.map((step, move) => {
+        const desc = move ?
+        'Go to move #' + move :
+        'Go to game start';
+        return (
+          <li>
+            <button 
+              onClick = { () =>  this.jumpTo(move) }
+            >
+              { desc }
+            </button>
+          </li>
+        );
+      });
+
       let status;
 
       if (winner) {
@@ -96,8 +112,8 @@ const Square = (props) => {
             />
           </div>
           <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
+            <div>{ status }</div>
+            <ol>{ moves }</ol>
           </div>
         </div>
       );
