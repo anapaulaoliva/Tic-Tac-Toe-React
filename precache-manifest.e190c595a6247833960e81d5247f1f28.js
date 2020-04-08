9af655cf191359e75b6740d1283b5623faa7d14d
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-04-08 10:33:49 -0500

    Updates

---
+++ precache-manifest.e190c595a6247833960e81d5247f1f28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f6669bbdfcf903736a8862a978ce81",
+    "url": "/Tic-Tac-Toe-React/index.html"
+  },
+  {
+    "revision": "32efbaf8e8a3a7f3edc0",
+    "url": "/Tic-Tac-Toe-React/static/css/main.55846d43.chunk.css"
+  },
+  {
+    "revision": "e42df836c807a06f08e6",
+    "url": "/Tic-Tac-Toe-React/static/js/2.a5e149bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tic-Tac-Toe-React/static/js/2.a5e149bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32efbaf8e8a3a7f3edc0",
+    "url": "/Tic-Tac-Toe-React/static/js/main.1c6cb404.chunk.js"
+  },
+  {
+    "revision": "c65e114c24341fbc04a6",
+    "url": "/Tic-Tac-Toe-React/static/js/runtime-main.93f257b6.js"
+  }
+]);
